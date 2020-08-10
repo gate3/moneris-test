f3d87c9dd4a4938b81a704f1988f558fb6b78cd9
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2020-08-10 10:42:01 +0100

    Moneris test initial commit

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
             <input type='text' name='IDEBIT_AMOUNT' value='100'> <!-- ($1.00) use cent values instead of dollar.cent format -->
             <input type='text' name='IDEBIT_MERCHNUM' value='from Moneris Solutions'>
             <input type='text' name='IDEBIT_CURRENCY' value='CA'>
-            <input type='text' name='IDEBIT_FUNDEDURL' value='/funded.php'>
-            <input type='text' name='IDEBIT_NOTFUNDEDURL' value='/not-funded.php'>
+            <input type='text' name='IDEBIT_FUNDEDURL' value='https://peaceful-journey-49078.herokuapp.com/funded.php'>
+            <input type='text' name='IDEBIT_NOTFUNDEDURL' value='https://peaceful-journey-49078.herokuapp.com/not-funded.php'>
             <input type='text' name='IDEBIT_MERCHLANG' value='en'>
             <input type='text' name='IDEBIT_VERSION' value='1'>
             <input type="submit" name="Submit" value="Submit to Gateway">
